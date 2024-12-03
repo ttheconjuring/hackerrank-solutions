ea8b431f00a9ee3ee7ace8e9341d1f6364cc2d08
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-12-03 16:37:15 +0200

    'Java Loops 1' done

---
+++ prepare/java/JavaLoops1.java
@@ -0,0 +1,22 @@
+package prepare.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaLoops1 {
+
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            for (int i = 1; i <= 10; i++) {
+                System.out.printf("%d x %d = %d%n", n, i, n * i);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}

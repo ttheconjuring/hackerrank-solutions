783ca6ab779866d4d72d5e81c7ca26ba13379e9d
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-12-03 16:31:31 +0200

    Welcome To Java! done

---
+++ prepare/java/WelcomeToJava.java
@@ -0,0 +1,12 @@
+package prepare.java;
+
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+    }
+
+}
